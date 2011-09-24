bb3465b4e1032105d80c68b1cda7d32a2c6787a6
Jose Antonio Ortega Ruiz <jao@gnu.org>
2011-09-24 02:48:33 +0200

    Back to clojure 1.3.0
    All tests passing.

--- project.clj
+++ project.clj
@@ -1,3 +1,3 @@
 (defproject com.bigml/closchema "0.0.2-SNAPSHOT"
   :description "Implements the JSON Schema specification."
-  :dependencies [[org.clojure/clojure "1.2.1"]])
+  :dependencies [[org.clojure/clojure "1.3.0"]])
