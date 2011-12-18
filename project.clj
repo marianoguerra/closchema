3508bd6e7c42c1aff88d3f26325138711409c2bf
Jose Antonio Ortega Ruiz <jao@gnu.org>
2011-12-18 23:43:46 +0100

    Release 0.1

--- project.clj
+++ project.clj
@@ -1,3 +1,3 @@
-(defproject com.bigml/closchema "0.0.2-SNAPSHOT"
+(defproject com.bigml/closchema "0.1"
   :description "Implements the JSON Schema specification."
   :dependencies [[org.clojure/clojure "1.3.0"]])
