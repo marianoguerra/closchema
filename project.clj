e8c97743e9af1b45284948299e343aaa5bdf320b
Charles Parker <charleslparker@Charless-MacBook-Air.local>
2012-01-20 08:34:32 -0800

    Project file nits

--- project.clj
+++ project.clj
@@ -1,5 +1,4 @@
-(defproject com.bigml/closchema "0.1.3"
+(defproject com.bigml/closchema "0.1.4"
   :description "Implements the JSON Schema specification."
   :dependencies [[org.clojure/clojure "1.3.0"]
-  		 [org.clojure/data.json "0.1.1"]]
-  :dev-dependencies [[lein-eclipse "1.0.0"]])
+                 [org.clojure/data.json "0.1.1"]]
