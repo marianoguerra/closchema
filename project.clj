a45fbd5305472db0856b9b9e83241465ceae9d21
Charles Parker <charleslparker@Charless-MacBook-Air.local>
2012-01-28 10:04:31 -0800

    Version number

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.bigml/closchema "0.1.6"
+(defproject com.bigml/closchema "0.1.7"
   :description "Implements the JSON Schema specification."
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.clojure/data.json "0.1.2"]])
