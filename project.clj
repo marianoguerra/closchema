89f75fccdecfe239f6d32130ed34972113484a52
Jose Antonio Ortega Ruiz <jao@gnu.org>
2012-07-20 20:58:10 +0200

    Clojure version set to 1.2

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject com.bigml/closchema "0.1.7"
   :description "Implements the JSON Schema specification."
-  :dependencies [[org.clojure/clojure "1.3.0"]
+  :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/data.json "0.1.2"]])
 
