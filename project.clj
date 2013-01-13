f0218444594db8bd31d634ffcb55d6f7aebbcc5e
Jose Antonio Ortega Ruiz <jao@gnu.org>
2013-01-13 14:36:12 +0100

    Version bumped to 0.2.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject bigml/closchema "0.1.9"
+(defproject bigml/closchema "0.2.0"
   :description "Implements the JSON Schema specification."
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.clojure/data.json "0.1.2"]])
